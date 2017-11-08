17df5458d25f0e45c5f3ccd1c382254775865083
Ulrich Thiel <thiel@mathematik.uni-stuttgart.de>
2017-11-09 08:42:26 +1100

    git subrepo clone https://github.com/picotech/picosdk-python-examples lib/picosdk-python-examples
    subrepo:
      subdir:   "lib/picosdk-python-examples"
      merged:   "e2c76ff"
    upstream:
      origin:   "https://github.com/picotech/picosdk-python-examples"
      branch:   "master"
      commit:   "e2c76ff"
    git-subrepo:
      version:  "0.3.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a7ee886"

---
+++ lib/picosdk-python-examples/python-picoscope/picoscope/__init__.py
@@ -0,0 +1,11 @@
+"""
+ *     Filename: __init__.py
+ *     
+ *	   Description:
+ *			Initialisation script.
+ * 	  
+ *	  @author: mario
+ *  
+ *    Copyright (C) 2014 - 2017 Pico Technology Ltd. See LICENSE file for terms.
+ *
+"""
